fdb70c829a5a32c00fe4c43b032b236f8c80693c
8kJordan <thepro321leyskil@gmail.com>
2026-04-01 13:52:23 -0400

    Initial Commit

---
+++ placemaking-iosTests/placemaking_iosTests.swift
@@ -0,0 +1,19 @@
+//
+//  placemaking_iosTests.swift
+//  placemaking-iosTests
+//
+//  Created by PC1 on 4/1/26.
+//
+
+import Testing
+@testable import placemaking_ios
+
+struct placemaking_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
